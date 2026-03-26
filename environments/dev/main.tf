4d26d843052440d2762fa43b4ddd1152bf4f9969
michael.garcia <mikejgarcia@gmail.com>
2026-03-26 14:28:07 -0600

    refactor: create QA environment configuration and update module references

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,3 +1,3 @@
-module "dev" {
+module "qa" {
     source = "../../modules/blog"
 }
---
+++ environments/qa/main.tf
@@ -0,0 +1,11 @@
+module "dev" {
+    source = "../../modules/blog"
+
+    environment= {
+        name ="qa"
+        network_prefix = "10.1"
+    }
+
+    min_size = 0  # save money do not provision
+    max_size = 1
+}
